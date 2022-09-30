f159540567f12265c5a2658cd4c17971b783fb2b
Premjith <47086427+premjithbenny@users.noreply.github.com>
2022-09-30 16:00:56 +0530

    Add files via upload

---
+++ aws-code/config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cjpfjdy5jiea.ap-south-1.rds.amazonaws.com"
+customuser = "premjith"
+custompass = "premjith123"
+customdb = "employee"
+custombucket = "webhosting-9203"
+customregion = "ap-south-1"
+
